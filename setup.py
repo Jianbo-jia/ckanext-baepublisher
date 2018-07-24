7b2b311210ee1d04d824fbcbf704b19f57d48ec0
Álvaro Arranz <aarranz@ficodes.com>
2018-07-24 14:11:30 +0200

    Bump plugin version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 import os
 
 
-version = '0.4'
+version = '0.5'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
